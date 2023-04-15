f737faaad7173ba5d6bbfbbbf93273059e7a6adc
d1mentor <gerasimenkot92@gmail.com>
2023-04-15 13:39:26 +0300

    init

---
+++ lib/parser/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Parser
+  VERSION = "0.1.0"
+end
---
+++ spec/parser_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Parser do
+  it "has a version number" do
+    expect(Parser::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
